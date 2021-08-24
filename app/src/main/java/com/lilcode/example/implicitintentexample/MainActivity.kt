417999c3d1c16aed56c00021b28dcf1803fff86e
NetLSS <futureprogrammer@naver.com>
2021-08-24 17:13:51 +0900

    Feat: 암시적 인텐트로 엑티비티 실행하기

--- app/src/main/java/com/lilcode/example/implicitintentexample/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/implicitintentexample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.lilcode.example.implicitintentexample
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.lilcode.example.implicitintentexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun showWebPage(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://whyprogrammer.tistory.com/"))
+        startActivity(intent)
     }
 }
